b25a095038204ea14ef985664f68970645ff9c14
Marleen <90028014+MV2005@users.noreply.github.com>
2022-11-08 18:18:14 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,9 +1,22 @@
-import inquirer from "inquirer";
+import inquirer from 'inquirer';
 inquirer
-.prompt([
-    {type:'input', name:'tag', message: 'How old are you?'}
+  .prompt([
+    {type:'input', name:'age', message: 'How old are you?'},
+    {type:'input', name:'name', message: 'Whats your name?'},
+    {type:'list', name:'gender', message: 'Whats your gender?', choices: ['female', 'male']},
+    {type:'checkbox', name:'hair', message: 'Whats your hair color?', choices: ['hall', 'blond','must','pruun']},
+    {type:'expand', name:'kumb', message: 'Which is better burger or pizza?', choices: [{ key: 'b', value: 'burger',}, { key: 'p', value: 'pizza',},]},
+    {type:'password', name:'secret', message: 'Tell me your secret'},
+    {type:'editor', name:'poem', message: 'Write me a poem?'},
+  ])
+  .then((answers) => {
+    console.log(`I am ${answers.age} years old`);
+    console.log(`My name is ${answers.name} `);
+    console.log(`My gender ${answers.gender} `);
+    console.log(`My hair color is ${answers.hair} `);
+    console.log(`Better is ${answers.kumb} `);
+    // node password.js 
+    console.log(`My secret is ${answers.secret} `);
+    console.log(`My poem ${answers.story} `);
 
-])
-.then((answers) => {
-    console.log('I am ' + answers.age + ' years old');
-})
+  })
